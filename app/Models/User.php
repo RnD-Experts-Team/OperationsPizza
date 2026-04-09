9b2b8e0f8c45e4300d7718a38999494f26af7438
anakin200a-ai <anakin200a@gmail.com>
2026-04-09 21:00:57 +0300

    u

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,47 +3,48 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens,HasFactory, Notifiable;
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable,HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+
     protected $fillable = [
+        'id',
         'name',
         'email',
-        'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
-        'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function createdSchedules()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(MasterSchedule::class, 'created_by');
+    }
+
+    public function publishedSchedules()
+    {
+        return $this->hasMany(MasterSchedule::class, 'published_by');
+    }
+
+    public function editedSchedules()
+    {
+        return $this->hasMany(Schedule::class, 'edited_by');
+    }
+    public function scheduleTemplates()
+    {
+        return $this->hasMany(ScheduleTemplate::class, 'created_by');
     }
 }
