d31e8bd54c2a3422f5b6505096894c4f86f98b48
anakin200a-ai <anakin200a@gmail.com>
2026-04-09 20:47:58 +0300

    add open api.json

--- routes/Api/dayOff.php
+++ routes/Api/dayOff.php
@@ -8,7 +8,6 @@ Route::post('days-off', [DayOffController::class, 'store']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('stores/{store}/days-off', [DayOffController::class, 'index']);
-    Route::get('stores/{store}/days-off/{day_off}', [DayOffController::class, 'show']);
     Route::put('stores/{store}/days-off/{day_off}', [DayOffController::class, 'update']);
     Route::delete('stores/{store}/days-off/{day_off}', [DayOffController::class, 'destroy']);
 });
